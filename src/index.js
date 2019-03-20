1e131568883335724b574568670d8055e22469ba
huangweiqi <huangweiqi@hzed.com>
2019-03-20 14:48:05 +0800

    加入 react-router-dom 复用

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import FastClick from 'fastclick';
-import Router from './router/';
+// import Router from './router/';
 import './assets/style/base.css';
-// import App from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 FastClick.attach(document.body);
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 // ReactDOM.render(<Router/>, document.getElementById('root'));
-const render = Component => {
-    ReactDOM.render(
-        <Component />,
-        document.getElementById('root')
-    )
-}
-
-render(Router);
+// const render = Component => {
+//     ReactDOM.render(
+//         <Component />,
+//         document.getElementById('root')
+//     )
+// }
+//
+// render(Router);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
