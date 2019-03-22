b7906fdade06a03aaae02725e9cbe45a4497a760
huangweiqi <huangweiqi@hzed.com>
2019-03-22 12:10:08 +0800

    项目初始化配置

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
-// import Tabbar from './components/Tabbar';
-import RouterMap from './router'
-import './static/iconfont.css'
-import './App.css';
+import logo from './logo.svg';
+import './App.scss';
 
 class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <RouterMap/>
-                {/*<div className="iconfont icon-home"></div>*/}
-
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p className="appText">
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
